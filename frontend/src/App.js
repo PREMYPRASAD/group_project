d3d63ed71418ac0d28f0c293a447cddb6f32b70a
Kavya Raj S B <kavyarajsb2000@gmail.com>
2023-05-04 15:07:15 +0530

    login page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,15 @@ import Contact from './Components/contact/contact';
 import Home from './Components/Home/Home';
 import Navbar from './Navbar/Navbar';
 import { Route,Routes } from "react-router-dom";
+import LoginPage from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
 
-    <Navbar/> 
       <Routes>
-          <Route path="/"  element={<Home/>} />
+          <Route path="/"  element={<LoginPage/>} />
+          <Route path="/home" element={< Home/>} />
           <Route path="/about" element={< About/>} />
           <Route path="/contactus" element={<Contact/>} />
    
